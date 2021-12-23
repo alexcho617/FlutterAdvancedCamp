9375965271d20516831ce005783e683f8c3478c5
growbook91 <growbook91@gmail.comvimtutor>
2021-12-23 14:44:51 +0900

    Deleting entries on cart page is completed.

--- lib/getxController/cartController.dart
+++ lib/getxController/cartController.dart
@@ -3,17 +3,39 @@
 import 'package:get/get.dart';
 import 'package:hemweb/model/product.dart';
 
+
 //define the getx controllers here
 class CartController extends GetxController {
   var cartList = <Product>[].obs;
   var checkList = <String>[].obs;
+  var allCheck = false;
 
   // Future<void> fetchFirestore()async {
   //   await
   // }
   addCart(Product item) {
-    cartList.add(item);
-    Get.snackbar('장바구니', '장바구니에 담겼습니다.',snackPosition: SnackPosition.BOTTOM);
+    if(!cartList.contains(item)){
+      item.isChecked = false;
+      cartList.add(item);
+      Get.snackbar('장바구니', '장바구니에 담겼습니다.',snackPosition: SnackPosition.BOTTOM);
+    }
+    else{
+      Get.snackbar('장바구니', '이미 장바구니에 존재하는 상품입니다.',snackPosition: SnackPosition.BOTTOM);
+    }
+
   }
 
+  void itemChange (bool value, int index) {
+    cartList[index].isChecked = value;
+    update();
+  }
+  void allChange (bool value) {
+    allCheck = value;
+    for(var item in cartList){
+      item.isChecked = value;
+      if(!checkList.contains(item.id))
+      checkList.add(item.id);
+    }
+    update();
+  }
 }
--- lib/screens/cart.dart
+++ lib/screens/cart.dart
@@ -10,15 +10,8 @@ import 'package:get/get_state_manager/src/rx_flutter/rx_getx_widget.dart';
 import 'package:hemweb/getxController/authController.dart';
 import 'package:hemweb/getxController/cartController.dart';
 
-class CartPage extends StatefulWidget {
+class CartPage extends StatelessWidget {
   const CartPage({Key? key}) : super(key: key);
-
-  @override
-  State<CartPage> createState() => _CartPageState();
-}
-
-class _CartPageState extends State<CartPage> {
-  var _isChecked = false;
 
   @override
   Widget build(BuildContext context) {
@@ -26,187 +19,176 @@ class _CartPageState extends State<CartPage> {
     return Scaffold(
       body: SafeArea(
           child: Center(
-            child: ListView(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 50.0, top: 30),
-                  child: Text(
-                    '장바구니',
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
+        child: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 50.0, top: 30),
+              child: Text(
+                '장바구니',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
                 ),
-                Divider(
-                  height: 50,
-                  indent: 0,
-                  endIndent: 0,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 20, horizontal: 50),
-                  child: Column(
+              ),
+            ),
+            Divider(
+              height: 50,
+              indent: 0,
+              endIndent: 0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Row(
-                            children: [
+                          GetBuilder<CartController>(builder: (_)=>
                               Checkbox(
-                                  value: _isChecked,
+                                  value: _.allCheck,
                                   onChanged: (value) {
-                                    setState(() {
-                                      _isChecked = value!;
-                                    });
-                                    print(_isChecked);
+                                    _.allChange(value!);
                                   }),
-                              Text('전체선택'),
-                            ],
                           ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              ElevatedButton(
-                                onPressed: () {
-                                  for (var item in cartController.checkList) {
-                                    cartController.cartList.removeWhere((
-                                        element) => element.id == item);
-                                    print(item);
-                                  }
-                                },
-                                child: Text(
-                                  '선택삭제',
-                                  style: TextStyle(color: Colors.black),
-                                ),
-                                style: ElevatedButton.styleFrom(
-                                  primary: Colors.white,
-                                ),
-                              ),
-                              ElevatedButton(
-                                onPressed: () {},
-                                child: Text(
-                                  '품절삭제',
-                                  style: TextStyle(color: Colors.black),
-                                ),
-                                style: ElevatedButton.styleFrom(
-                                  primary: Colors.white,
-                                ),
-                              ),
-                            ],
+                          Text('전체선택'),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              for (var item in cartController.checkList) {
+                                cartController.cartList.removeWhere(
+                                    (element) => element.id == item);
+                                // cartController.checkList.remove(item);
+                                print(item);
+                              }
+                            },
+                            child: Text(
+                              '선택삭제',
+                              style: TextStyle(color: Colors.black),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.white,
+                            ),
                           ),
                         ],
                       ),
-                      Divider(
-                        height: 50,
-                        indent: 0,
-                        endIndent: 0,
-                        thickness: 2,
-                        color: Colors.black,
-                      ),
-
-                      Text(
-                        '상품정보',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-
-                      Divider(
-                        height: 50,
-                        indent: 0,
-                        endIndent: 0,
-                        thickness: 2,
-                      ),
-
-                      Obx(() =>
-                          cartController.cartList.isNotEmpty ?
-                      ListView.builder(
-                        physics: const NeverScrollableScrollPhysics(),
-                        shrinkWrap: true,
-                        itemCount: cartController.cartList.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          return ProductTile(
-                            imageUrl: cartController.cartList[index]
-                                .imageURL,
-                            name: cartController.cartList[index].name,
-                            price: cartController.cartList[index].price,
-                            brand: cartController.cartList[index]
-                                .company,
-                            index: cartController.cartList[index].id,);
-                        },
-                      )
-                          :
-                      Center(
-                        child: Column(
-                          children: [
-                            SizedBox(
-                              height: 50,
-                            ),
-                            Icon(
-                              Icons.shopping_cart_outlined,
-                              size: 50,
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Text(
-                                '장바구니에 상품을 담아보세요.',
-
-                              ),
-                            ),
-                            ElevatedButton(
-                              onPressed: () {
-                                Get.to(HomePage());
-                              },
-                              child: Text(
-                                '쇼핑 계속하기',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black,
-                                ),
-                              ),
-                              style: ElevatedButton.styleFrom(
-                                primary: Colors.white,
-                                padding: EdgeInsets.all(20),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      ),
-                      //1. 체크되고 삭제 되었을 때에?
-                      //2. x를 눌렀을 때
-
-
                     ],
                   ),
-                ),
-              ],
+                  Divider(
+                    height: 50,
+                    indent: 0,
+                    endIndent: 0,
+                    thickness: 2,
+                    color: Colors.black,
+                  ),
+
+                  Text(
+                    '상품정보',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+
+                  Divider(
+                    height: 50,
+                    indent: 0,
+                    endIndent: 0,
+                    thickness: 2,
+                  ),
+
+                  Obx(
+                    () => cartController.cartList.isNotEmpty
+                        ? ListView.builder(
+                            physics: const NeverScrollableScrollPhysics(),
+                            shrinkWrap: true,
+                            itemCount: cartController.cartList.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              return ProductTile(
+                                //check도.
+                                imageUrl:
+                                    cartController.cartList[index].imageURL,
+                                name: cartController.cartList[index].name,
+                                price: cartController.cartList[index].price,
+                                brand: cartController.cartList[index].company,
+                                id: cartController.cartList[index].id,
+                                //id는 왜 필요하고 index는
+                                index: index,
+                              );
+                            },
+                          )
+                        : Center(
+                            child: Column(
+                              children: [
+                                SizedBox(
+                                  height: 50,
+                                ),
+                                Icon(
+                                  Icons.shopping_cart_outlined,
+                                  size: 50,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    '장바구니에 상품을 담아보세요.',
+                                  ),
+                                ),
+                                ElevatedButton(
+                                  onPressed: () {
+                                    Get.back();
+                                  },
+                                  child: Text(
+                                    '쇼핑 계속하기',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  style: ElevatedButton.styleFrom(
+                                    primary: Colors.white,
+                                    padding: EdgeInsets.all(20),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                  ),
+                  //1. 체크되고 삭제 되었을 때에?
+                  //2. x를 눌렀을 때
+                ],
+              ),
             ),
-          )),
+          ],
+        ),
+      )),
     );
   }
 }
 
-class ProductTile extends StatefulWidget {
-
-  ProductTile(
-      {required this.imageUrl, required this.name, required this.price, required this.brand, required this.index});
+class ProductTile extends StatelessWidget {
+  ProductTile({
+    required this.imageUrl,
+    required this.name,
+    required this.price,
+    required this.brand,
+    required this.index,
+    required this.id,
+  });
 
   String imageUrl;
   String name;
   String price;
   String brand;
-  String index;
+  String id;
+  int index;
 
-  @override
-  State<ProductTile> createState() => _ProductTileState();
-}
-
-class _ProductTileState extends State<ProductTile> {
   final CartController cartController = Get.put(CartController());
-  var _isChecked = false;
 
-  //여기에 함수를 만들어서 controller에 접근 가능..?
-  //아 그냥 여기서는 index목록을 추가하고 삭제를 나중에 해도 될 것 같은데???
+//여기에 함수를 만들어서 controller에 접근 가능..?
+//아 그냥 여기서는 index목록을 추가하고 삭제를 나중에 해도 될 것 같은데???
+//여기서 checklist를 추가..
 
   @override
   Widget build(BuildContext context) {
@@ -217,42 +199,54 @@ class _ProductTileState extends State<ProductTile> {
           children: [
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Checkbox(
-                  value: _isChecked,
-                  onChanged: (value) {
-                    setState(() {
-                      _isChecked = value!;
+              child: GetBuilder<CartController>(
+                builder: (_) => Checkbox(
+                    //여기서 ischecked가 cartlist꺼야 해...그럴려면...
+                    value: _.cartList[index].isChecked,
+                    onChanged: (value) {
+                      // print('value before : $value');
+                      // print('ischeckd before : ${_.cartList[index].isChecked}');
+                      _.itemChange(value!, index);
+                      // value = cartController.cartList[index].isChecked;
+                      // print('value after : $value');
+                      // print('ischeckd after : ${_.cartList[index].isChecked}');
                       if (value == true) {
-                        cartController.checkList.add(widget.index);
-                        print(cartController.checkList);
+                        _.checkList.add(id);
+                        print(_.checkList);
+                      } else {
+                        _.checkList
+                            .removeWhere((element) => element == id);
+                        print(_.checkList);
+
                       }
-                      else {
-                        cartController.checkList.removeWhere((
-                            element) => element == widget.index);
-                        print(cartController.checkList);
-                      }
-                    });
-                    print(_isChecked);
-                  }),
+
+                      print(_.cartList[index].isChecked);
+                    }),
+              ),
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Container(child: Image.network(
-                widget.imageUrl, width: 100, height: 100, fit: BoxFit.fill,)),
+              child: Container(
+                  child: Image.network(
+                imageUrl,
+                width: 100,
+                height: 100,
+                fit: BoxFit.fill,
+              )),
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(widget.brand),
-                  Text(widget.name + "\n" + widget.price + '원',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold, fontSize: 18),),
+                  Text(brand),
+                  Text(
+                    name + "\n" + price + '원',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
                 ],
               ),
             ),
-
           ],
         ),
         IconButton(onPressed: () {}, icon: Icon(Icons.cancel)),
